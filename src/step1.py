4c53b77fbd77ce35f814f8ab8e6dd551d7ef30ad
Frederic Stark <fstark@kds.com>
2017-12-15 21:49:13 +0100

    bs4 test

--- src/step1.py
+++ src/step1.py
@@ -1,3 +1,19 @@
 #!/usr/local/bin/python3
 
 from bs4 import BeautifulSoup
+
+html_doc = """
+<html>
+    <head>
+    <title>Titre de votre site</title>
+    </head>
+    <body>
+        <p>Texte à lire 1</p>
+        <p>Texte à lire 2</p>
+    </body>
+</html>
+"""
+soup = BeautifulSoup(html_doc)
+    
+for p in soup.find_all('p'):
+    print( p )
