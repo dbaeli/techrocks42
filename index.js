ea97d4fcf4ab89edcaa2823d8742865185595e90
dba <dba@lesfurets.com>
2017-12-16 01:35:42 +0100

    wappalyzer

---
+++ index.js
@@ -0,0 +1,26 @@
+let Wappalyzer = require('wappalyzer');
+const options = {
+  debug: false,
+  delay: 500,
+  maxDepth: 3,
+  maxUrls: 10,
+  maxWait: 1000,
+  recursive: true,
+  requestTimeout: 3000,
+  userAgent: 'Wappalyzer',
+};
+
+const wappalyzer = new Wappalyzer('https://www.wappalyzer.com', options);
+
+wappalyzer.analyze()
+  .then(json => {
+    process.stdout.write(JSON.stringify(json, null, 2) + '\n')
+ 
+    process.exit(0);
+  })
+  .catch(error => {
+    process.stderr.write(error + '\n')
+ 
+    process.exit(1);
+});
+
